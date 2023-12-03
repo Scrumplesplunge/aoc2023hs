b1cb77cd94472fa4dbe0e91a48c4d230dec3030a
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-03 10:56:46 +0000

    Clean up day 3.

--- src/day03.hs
+++ src/day03.hs
@@ -2,54 +2,51 @@ import Control.Monad.State
 import Data.Char
 import Data.List
 
--- expandX maps a list bools to another list of bools where bool i is true if
--- position i is within 1 space of a position that was true in the input, e.g.
---      #...#..
---   -> ##.###.
+-- expandX and expand are convolution filters which make each cell the maximum
+-- of itself and its immediate neighbours.
 expandX :: (Ord a, Bounded a) => [a] -> [a]
 expandX = go . (minBound :)
   where go [_] = []
         go xs = maximum (take 3 xs) : go (tail xs)
-
 expand :: (Ord a, Bounded a) => [[a]] -> [[a]]
 expand = transpose . map expandX . transpose . map expandX
 
-part1 input = sum . map (sum . row) $ grid
+int :: String -> Int
+int = foldl' (\a x -> 10 * a + digitToInt x) 0
+
+part1 input = sum . map (sum . rowIds) $ grid
   where isSym c = c /= '.' && not (isDigit c)
         grid = zipWith zip input $ expand $ map (map isSym) input
-        row [] = []
-        row xs = let (ns, xs') = span (isDigit . fst) xs
-                     isPartNumber = any snd ns
-                     n = foldl' (\a x -> 10 * a + digitToInt x) 0 (map fst ns)
-                     xs'' = dropWhile (not . isDigit . fst) xs'
-                 in if isPartNumber then n : row xs'' else row xs''
+        rowIds [] = []
+        rowIds xs =
+          let (ns, xs') = span (isDigit . fst) xs
+              isPartNumber = any snd ns
+              n = int (map fst ns)
+              xs'' = dropWhile (not . isDigit . fst) xs'
+          in if isPartNumber then n : rowIds xs'' else rowIds xs''
 
-part2 input = sumGroups . filter ((/= 0) . fst) . concat . map row $ grid'
-  where nextId :: State Int Int
-        nextId = do
-          id <- get
-          modify (+1)
-          return id
-        makeRow :: String -> State Int [Int]
-        makeRow [] = return []
-        makeRow ('*' : xs) = do
-          id <- nextId
-          rest <- makeRow xs
-          return (id : rest)
-        makeRow (x : xs) = makeRow xs >>= (return . (0 :))
-        makeGrid :: [String] -> State Int [[Int]]
-        makeGrid input = mapM makeRow input
-        grid :: [[Int]]
-        grid = fst $ runState (makeGrid input) 1
-        grid' = zipWith zip input $ expand grid
-        row :: [(Char, Int)] -> [(Int, Int)]
-        row [] = []
-        row xs = let (ns, xs') = span (isDigit . fst) xs
-                     id = maximum (map snd ns)
-                     n = foldl' (\a x -> 10 * a + digitToInt x) 0 (map fst ns)
-                     xs'' = dropWhile (not . isDigit . fst) xs'
-                 in if null ns then row xs'' else (id, n) : row xs''
+part2 input = sumGroups . filter ((/= 0) . fst) . concat . map rowIds $ grid
+  where -- Build a grid where each '*' is annotated with a unique ID starting at
+        -- 1, and each character is annotated with the (assumed unique) adjacent
+        -- '*'.
+        getId :: Char -> State Int Int
+        getId '*' = do { id <- get; modify (+1); return id }
+        getId _ = return 0
+        grid :: [[(Char, Int)]]
+        grid = zipWith zip input $ expand $ fst $
+               runState (mapM (mapM getId) input) 1
+        -- Parse the part numbers in a line, tagged with gear IDs.
+        rowIds :: [(Char, Int)] -> [(Int, Int)]
+        rowIds [] = []
+        rowIds xs = let (ns, xs') = span (isDigit . fst) xs
+                        id = maximum (map snd ns)
+                        n = int (map fst ns)
+                        xs'' = dropWhile (not . isDigit . fst) xs'
+                    in if null ns then rowIds xs'' else (id, n) : rowIds xs''
+        -- Group the part numbers by the corresponding gear.
         groupIds = groupBy (\a b -> fst a == fst b) . sortOn fst
+        -- Calculate the product for each gear with exactly two part numbers and
+        -- sum the results.
         sumGroups =
           sum . map (product . map snd) . filter ((>1) . length) . groupIds
 
