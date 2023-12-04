1d3c1cc9d9d05831ee4ce86d4ce5e9c077086064
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-04 13:03:12 +0000

    Iterate backwards instead of forwards.

--- src/day04.hs
+++ src/day04.hs
@@ -1,23 +1,20 @@
-import Data.Bits
 import Data.List
 
+-- Parse the number of winning numbers from each line.
 parse :: String -> [Int]
-parse = map card . lines
-  where card = wins . break ((== '|') . head) . words . drop 10
-        wins (ws, ("|" : ns)) = length (ws `intersect` ns)
+parse = map (wins . break ((== '|') . head) . words . drop 10) . lines
+  where wins (ws, ("|" : ns)) = length (ws `intersect` ns)
 
-part1 :: Int -> Int
-part1 n = if n == 0 then 0 else 1 `shiftL` (n - 1)
+-- Sum the scores for each card.
+part1 :: [Int] -> Int
+part1 = let score n = if n == 0 then 0 else 2 ^ (n - 1) in sum . map score
 
+-- Iterate backwards to build a list where the ith element contains the number
+-- of cards won transitively by a single copy of the ith card.
 part2 :: [Int] -> Int
-part2 = go 0 . zip [1 | _ <- [0..]]
-  where go n [] = n
-        go n ((c, s) : cs) = go (n + c) (acc c s cs)
-        acc _ _ [] = []
-        acc _ 0 cs = cs
-        acc c s ((c', s') : cs) = (c' + c, s') : acc c (s - 1) cs
+part2 = sum . foldr (\w cs -> 1 + sum (take w cs) : cs) []
 
 main = do
   cs <- parse <$> getContents
-  putStrLn $ show $ sum $ map part1 cs
+  putStrLn $ show $ part1 cs
   putStrLn $ show $ part2 cs
