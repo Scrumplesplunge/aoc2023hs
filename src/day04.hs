61061ac24fa37a8e6f32252b6d4f8483204ea706
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-04 12:38:12 +0000

    Remove an unused as pattern

--- src/day04.hs
+++ src/day04.hs
@@ -12,7 +12,7 @@ part1 n = if n == 0 then 0 else 1 `shiftL` (n - 1)
 part2 :: [Int] -> Int
 part2 = go 0 . zip [1 | _ <- [0..]]
   where go n [] = n
-        go n x@((c, s) : cs) = go (n + c) (acc c s cs)
+        go n ((c, s) : cs) = go (n + c) (acc c s cs)
         acc _ _ [] = []
         acc _ 0 cs = cs
         acc c s ((c', s') : cs) = (c' + c, s') : acc c (s - 1) cs
