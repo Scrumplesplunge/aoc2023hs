3eea5406e948c3f9f70c0ec624a47f19d761ac44
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-05 06:42:34 +0000

    Day 5, first thing that worked.

---
+++ src/day05.hs
@@ -0,0 +1,68 @@
+import Debug.Trace
+import Data.Char
+import Data.List
+
+type Seed = Int
+type Map = [(Int, Int, Int)]
+type Input = ([Seed], [Map])
+
+nat :: String -> Int
+nat = foldl' (\a x -> 10 * a + digitToInt x) 0
+
+parse :: String -> Input
+parse = sections . lines
+  where sections :: [String] -> Input
+        sections (s : "" : ms) = (seeds s, makeMaps [] ms)
+        seeds :: String -> [Seed]
+        seeds = map nat . tail . words
+        makeMaps :: [String] -> [String] -> [Map]
+        makeMaps m [] = [makeMap (reverse m)]
+        makeMaps m ("" : ms) = makeMap (reverse m) : makeMaps [] ms
+        makeMaps m (m' : ms) = makeMaps (m' : m) ms
+        makeMap :: [String] -> Map
+        makeMap = map makeEntry . tail
+        makeEntry :: String -> (Int, Int, Int)
+        makeEntry e = let [a, b, c] = map nat $ words e in (a, b, c)
+
+lookupOne :: Map -> Int -> Int
+lookupOne ((d, s, n) : ms) x | s <= x && x < s + n = d - s + x
+lookupOne (_ : ms) x = lookupOne ms x
+lookupOne [] x = x
+
+part1 :: Input -> Int
+part1 (ss, ms) = minimum $ foldl (\ss m -> map (lookupOne m) ss) ss ms
+
+lookupRange :: Map -> (Int, Int) -> [(Int, Int)]
+-- Case where the search range is empty.
+lookupRange _ (ro, 0) = []
+-- Case where the start of the input range is inside a map range.
+lookupRange ((d, s, n) : ms) (ro, rn) | s <= ro && ro < s + n =
+    let start = ro
+        end = min (s + n) (ro + rn)
+    in (start - s + d, end - start) : lookupRange ms (end, ro + rn - end)
+-- Case where the end of the input range is inside a map range.
+lookupRange ((d, s, n) : ms) (ro, rn) | s < ro + rn && ro + rn <= s + n =
+    let ro' = max s ro            -- Actual overlap start.
+        rn' = ro + rn - ro'       -- Actual overlap size.
+    in (ro' - s + d, rn') : lookupRange ms (ro, ro' - ro)
+-- Case where the range does not overlap with this map entry.
+lookupRange (_ : ms) (ro, rn) = lookupRange ms (ro, rn)
+lookupRange [] (ro, rn) = [(ro, rn)]
+
+debug ranges = show n ++ " in " ++ show ranges
+  where n = sum $ map snd ranges
+        min = minimum $ map fst ranges
+        max = maximum $ map (\(o, n) -> o + n) ranges
+
+-- 63651375 too high
+part2 :: Input -> Int
+part2 (ss, ms) = minimum $ map fst $ foldl mapRanges (makeSeedRange ss) ms
+  where mapRanges sr m = let x = concat $ map (lookupRange m) sr
+                         in trace (debug sr ++ " -> " ++ debug x) x
+        makeSeedRange (o : n : ss) = (o, n) : makeSeedRange ss
+        makeSeedRange [] = []
+
+main = do
+  input <- parse <$> getContents
+  putStrLn $ show $ part1 input
+  putStrLn $ show $ part2 input
