31da848e28c03bb682d34c537ffacc2c868774ac
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-12 15:04:41 +0000

    Make `line` a chars instead of tails: 61ms -> 57ms.

--- src/day12.hs
+++ src/day12.hs
@@ -12,22 +12,21 @@ parse = map (entry . words) . lines
 arrange :: String -> [Int] -> Int
 arrange l cs = a ! (0, 0)
   where (n, m) = (length l, length cs)
-        line = array (0, n) $ zip [0..] $ tails l
+        line = array (0, n - 1) $ zip [0..] l
         counts = array (0, m - 1) $ zip [0..] cs
+        bad i = line ! i == '#'
         a = array ((0, 0), (n, m)) [((i, j), f i j) | j <- [0..m], i <- [0..n]]
-        f i j
-          | j == m                 = if any (== '#') (line ! i) then 0 else 1
-          | i == n                 = 0
-          | head (line ! i) == '.' = a ! (i + 1, j)
-          | otherwise              = here + after
+        f i j | j == m          = if any bad [i..n-1] then 0 else 1
+              | i == n          = 0
+              | line ! i == '.' = a ! (i + 1, j)
+              | otherwise       = here + after
           where c = counts ! j
                 i' = i + c
-                p = take c (line ! i)
-                canFit = i' <= n && not (any (== '.') p)
-                canStop = i' == n || head (line ! i') /= '#'
+                canFit = i' <= n && not (any ((=='.') . (line!)) [i..i+c-1])
+                canStop = i' == n || line ! i' /= '#'
                 poss = canFit && canStop
                 here = if poss then a ! (min (i' + 1) n, j + 1) else 0
-                after = if head (line ! i) == '?' then a ! (i + 1, j) else 0
+                after = if line ! i == '?' then a ! (i + 1, j) else 0
 
 part1 :: [(String, [Int])] -> Int
 part1 = sum . map (uncurry arrange)
