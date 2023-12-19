53962285ba94be4b0a975a7249d021d4952a4ef2
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-19 14:54:02 +0000

    Small performance improvement.

--- src/day17.hs
+++ src/day17.hs
@@ -31,39 +31,35 @@ measureLoss grid n [] = []
 measureLoss grid n (x@pos : xs) =
   let n' = n + grid ! pos in (n', x) : measureLoss grid n' xs
 
-options :: (Int, Int) -> Grid -> Int -> ((Int, Int), Direction)
-        -> [(Int, ((Int, Int), Direction))]
-options (minSteps, maxSteps) grid n (pos, d) = do
-  d <- directions d
-  let steps = take maxSteps $ tail $ iterate (move d) pos
-      reachable = filter (inRange (bounds grid)) steps
-  (n', pos) <- drop (minSteps - 1) $ measureLoss grid n reachable
-  return (n', (pos, d))
-
-run :: (Int, Int) -> Grid -> STArray s ((Int, Int), Direction) Int
-    -> Set (Int, ((Int, Int), Direction)) -> ST s ()
-run steps grid a frontier | Set.null frontier = return ()
-run steps grid a frontier = do
-  let ((n, x), frontier') = Set.deleteFindMin frontier
-  n' <- readArray a x
-  if n' >= 0 then
-    run steps grid a frontier'
-  else do
-    writeArray a x n
-    let ns = options steps grid n x
-    run steps grid a (frontier' `Set.union` Set.fromList ns)
-
-explore :: (Int, Int) -> Grid -> ST s (STArray s ((Int, Int), Direction) Int)
-explore steps grid = do
-  let (_, (w, h)) = bounds grid
-  a <- newArray (((1, 1), U), ((w, h), R)) (-1)
-  run steps grid a (Set.fromList [(0, ((1, 1), R)), (0, ((1, 1), D))])
-  return a
-
 solve :: (Int, Int) -> Grid -> Int
-solve steps grid = minimum $ filter (>=0) [a ! ((w, h), d) | d <- [U, D, L, R]]
-  where (_, (w, h)) = bounds grid
-        a = runSTArray (explore steps grid)
+solve (minSteps, maxSteps) grid = runST explore
+  where (_, end) = bounds grid
+        options :: Int -> ((Int, Int), Direction)
+                -> [(Int, ((Int, Int), Direction))]
+        options n (pos, d) = do
+          d <- directions d
+          let steps = take maxSteps $ tail $ iterate (move d) pos
+              reachable = filter (inRange (bounds grid)) steps
+          (n', pos) <- drop (minSteps - 1) $ measureLoss grid n reachable
+          return (n', (pos, d))
+        explore :: ST s Int
+        explore = do
+          seen <- newArray (((1, 1), U), (end, R)) False
+          run seen (Set.fromList [(0, ((1, 1), R)), (0, ((1, 1), D))])
+        run :: STArray s ((Int, Int), Direction) Bool
+            -> Set (Int, ((Int, Int), Direction))
+            -> ST s Int
+        run seen frontier = do
+          let ((n, x), frontier') = Set.deleteFindMin frontier
+              ns = Set.fromList (options n x)
+          alreadySeen <- readArray seen x
+          if fst x == end then
+            return n
+          else if alreadySeen then
+            run seen frontier'
+          else do
+            writeArray seen x True
+            run seen (Set.union frontier' ns)
 
 main = do
   input <- parse <$> getContents
