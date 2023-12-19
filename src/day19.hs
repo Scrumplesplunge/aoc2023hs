3b1ec2c248c8f650c7ced837879795f9c3753bb4
Joe Fowler <scrumplesplunge@gmail.com>
2023-12-19 13:11:49 +0000

    Share code for part1 and part2.

--- src/day19.hs
+++ src/day19.hs
@@ -5,11 +5,15 @@ import Data.List
 
 data Cond = Lt Char Int | Gt Char Int | Always deriving Show
 data Action = Accept | Reject | Jump String deriving Show
+-- Name of the workflow, plus a list of conditions and actions where the last
+-- condition must be `Always`.
 type Workflow = (String, [(Cond, Action)])
-data Part a = Part a a a a deriving Show
+-- Abstract representation of a part with an inclusive range of values for each
+-- of the categories.
+data Part = Part (Int, Int) (Int, Int) (Int, Int) (Int, Int) deriving Show
 
 -- Map one part to another by modifying the value of one category.
-pmap :: Char -> (a -> a) -> Part a -> Part a
+pmap :: Char -> ((Int, Int) -> (Int, Int)) -> Part -> Part
 pmap 'x' f (Part x m a s) = Part (f x) m a s
 pmap 'm' f (Part x m a s) = Part x (f m) a s
 pmap 'a' f (Part x m a s) = Part x m (f a) s
@@ -45,7 +49,7 @@ workflow = do
   char '}'
   return (name, cs ++ [(Always, a)])
 
-part :: Parser (Part Int)
+part :: Parser Part
 part = do
   string "{x="
   x <- int
@@ -56,66 +60,55 @@ part = do
   string ",s="
   s <- int
   string "}"
-  return (Part x m a s)
+  return (Part (x, x) (m, m) (a, a) (s, s))
 
-parseInput :: String -> ([Workflow], [Part Int])
+parseInput :: String -> ([Workflow], [Part])
 parseInput = finish . break (null . snd) . zip [1..] . lines
-  where finish :: ([(Int, String)], [(Int, String)]) -> ([Workflow], [Part Int])
+  where finish :: ([(Int, String)], [(Int, String)]) -> ([Workflow], [Part])
         finish (ws, (_, "") : ps) = (map (f workflow) ws, map (f part) ps)
         f :: Parser a -> (Int, String) -> a
         f p (n, x) = case parse p ("input:" ++ show n) x of
           Left e -> error (show e)
           Right x -> x
 
-part1 :: ([Workflow], [Part Int]) -> Int
-part1 (ws, ps) = sum $ map score $ filter (check (get "in")) ps
-  where score (Part x m a s) = x + m + a + s
-        get w = case lookup w ws of
-          Nothing -> error "Bad workflow reference"
-          Just x -> x
-        cat 'x' (Part x m a s) = x
-        cat 'm' (Part x m a s) = m
-        cat 'a' (Part x m a s) = a
-        cat 's' (Part x m a s) = s
-        check :: [(Cond, Action)] -> Part Int -> Bool
-        check ((Always, a) : _) p = act a p
-        check ((Lt c n, a) : xs) p = if cat c p < n then act a p else check xs p
-        check ((Gt c n, a) : xs) p = if cat c p > n then act a p else check xs p
-        act :: Action -> Part Int -> Bool
-        act Accept p = True
-        act Reject p = False
-        act (Jump w) p = check (get w) p
-
-type Value = (Int, Int)  -- Inclusive range.
-
-possible :: Part Value -> Bool
+possible :: Part -> Bool
 possible (Part x m a s) = not (any (\(l, u) -> u < l) [x, m, a, s])
 
 -- Splits a value into options that evaluate to false and options that evaluate
 -- to true under the given condition.
-fork :: Cond -> Part Value -> (Part Value, Part Value)
+fork :: Cond -> Part -> (Part, Part)
 fork Always p = (Part (2, 1) (2, 1) (2, 1) (2, 1), p)
 fork (Lt c n) p = (pmap c (\(l, u) -> (max l n, u)) p,        -- c >= n
                    pmap c (\(l, u) -> (l, min u (n - 1))) p)  -- c < n
 fork (Gt c n) p = (pmap c (\(l, u) -> (l, min u n)) p,        -- c <= n
                    pmap c (\(l, u) -> (max l (n + 1), u)) p)  -- c > n
 
--- part2 :: ([Workflow], [Part Int]) -> Int
-part2 (ws, ps) = sum $ map count $ check (get "in") (Part val val val val)
-  where val = (1, 4000)
-        get w = case lookup w ws of
+-- Given an initial abstract part, returns a list of disjoint abstract parts
+-- which are each a subset of the input and all satisfy the workflow.
+eval :: [Workflow] -> Part -> [Part]
+eval ws = check (get "in")
+  where get w = case lookup w ws of
           Nothing -> error "Bad workflow reference"
           Just x -> x
-        check :: [(Cond, Action)] -> Part Value -> [Part Value]
+        check :: [(Cond, Action)] -> Part -> [Part]
         check ((c, a) : xs) p | possible p = let (f, t) = fork c p
                                              in check xs f ++ act a t
                               | otherwise  = []
         check [] p | possible p = error "Fell off the workflow!"
                    | otherwise  = []
-        act :: Action -> Part Value -> [Part Value]
-        act Accept p = [p]
+        act :: Action -> Part -> [Part]
+        act Accept p | possible p = [p]
+                     | otherwise = []
         act Reject p = []
         act (Jump w) p = check (get w) p
+
+part1 :: ([Workflow], [Part]) -> Int
+part1 (ws, ps) = sum $ map score $ filter (not . null . eval ws) ps
+  where score (Part (x, _) (m, _) (a, _) (s, _)) = x + m + a + s
+
+part2 :: ([Workflow], [Part]) -> Int
+part2 (ws, ps) = sum $ map count $ eval ws (Part any any any any)
+  where any = (1, 4000)
         count p@(Part x m a s) = size x * size m * size a * size s
         size (l, u) = max 0 (u - l + 1)
 
